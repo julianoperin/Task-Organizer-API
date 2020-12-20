5f4f29fa66c7ff923838b28b1de3dd5a3cb0b661
‘Juliano <julianoperins@gmail.com>
2020-12-20 14:17:11 -0500

    Inital commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://julianoperin:123@task-organizer.yb7r4.mongodb.net/task-organizer?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+//! TASKS MODEL
+const Tasks = mongoose.model("Tasks", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+//! USER
+// const me = new User({
+//   name: "Dennis",
+//   email: "jonas@gmail.com",
+//   password: "tes",
+//   age: 51,
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((err) => console.log("Error", err));
+
+//! TASK
+// const task = new Tasks({
+//   description: "Take the              ",
+//   completed: false,
+// });
+
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((err) => console.log("error", err));
